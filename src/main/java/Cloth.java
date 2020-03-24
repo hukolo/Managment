f65dbf20cc28b5e9a027bcec7e7183ab48a41c8d
hukolo <hukolo@gmail.com>
2020-03-24 22:25:51 +0100

    Switch project to Maven

---
+++ src/main/java/Cloth.java
@@ -0,0 +1,28 @@
+public class Cloth extends Product{
+    String size;
+    String material;
+
+    public Cloth(int id,String productName, float price, float weight, String color,int productCount, String size, String material){
+        super(id,productName,price,weight,color,productCount);
+        this.size = size;
+        this.material = material;
+    }
+
+    public String getSize(){
+        return size;
+    }
+
+    public String getMaterial(){
+        return material;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Cloth{" +
+                "size='" + size + '\'' +
+                ", material='" + material + '\'' +
+                '}';
+    }
+
+
+}
---
+++ src/main/java/Product.java
@@ -0,0 +1,64 @@
+public class Product {
+    private int id;
+    private String productName;
+    private float price;
+    private float weight;
+    private String color;
+    private int productCount;
+
+    public Product(int id, String productName, float price, float weight,String color, int productCount){
+        this.id = id;
+        this.productName = productName;
+        this.price = price;
+        this.weight = weight;
+        this.color = color;
+        this.productCount = productCount;
+    }
+
+    public void setPrice(float price){
+        this.price = price;
+    }
+
+    public void setProductCount(int productCount){
+        this.productCount = productCount;
+    }
+
+    public String getProductName(){
+        return productName;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public float getPrice(){
+        return price;
+    }
+
+    public float getWeight(){
+        return weight;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public int getProductCount(){
+        return productCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", productName='" + productName + '\'' +
+                ", price=" + price +
+                ", weight=" + weight +
+                ", color='" + color + '\'' +
+                ", productCount=" + productCount +
+                '}';
+    }
+
+
+
+}
